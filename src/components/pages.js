a3c6513721b8d3d7613869434d4c0cfc4157a233
Jocelyn Remington <jocelynrem@gmail.com>
2024-06-17 13:25:00 -0400

    skills

--- src/components/pages.js
+++ src/components/pages.js
@@ -192,7 +192,7 @@ export function Resume() {
                 <a href="https://docs.google.com/document/d/1ISDJDI6HHXNktBTEYzDdGsEXPZqZwd_1rlvw95BCUHc/edit?usp=sharing" target="_blank" rel="noreferrer"><img src={resume} className="w-screen object-cover hover:scale-105 transition-all transform duration-400 rounded-md max-w-60 max-h-60 md:float-right" alt="resume" /></a>
                 <div className="flex-col">
                     <h1 className=" pt-2 pl-5 text-3xl">SKILLS:</h1>
-                    <p className=" pt-2 pl-5 font-thin text-3xl">| Photoshop | Javascript | HTML5 | CSS | React |  Bootstrap | Tailwind | Adobe XD | MySQL | MongoDB | GraphQL</p>
+                    <p className=" pt-2 pl-5 font-thin text-3xl">| WCAG 2.1 | Figma | Adobe CC | Javascript | HTML5 | CSS | React | Python | Django |  Bootstrap | Tailwind | Python | </p>
                 </div>
 
             </div>
